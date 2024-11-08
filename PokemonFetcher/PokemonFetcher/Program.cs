07ff127ba052e418fdfc32355cb71fd067646930
lGruenzweilHTL <lukas.gruenzweil@liwest.at>
2024-11-08 09:49:36 +0100

    Add sub-pages to generator

--- PokemonFetcher/PokemonFetcher/Program.cs
+++ PokemonFetcher/PokemonFetcher/Program.cs
@@ -1,4 +1,9 @@
-﻿using System.Text.Json.Nodes;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json.Nodes;
 
 namespace PokemonFetcher;
 
@@ -6,25 +11,33 @@ class Program
 {
     static void Main(string[] args)
     {
+        if (!ArgsValid(args, out string mainPagePath, out string subPageDirectory, out string stylesheetPath))
+        {
+            return;
+        }
+
+        // Build main page
         var jsonObject = FetchPokemon(0, 151);
         var pokemonArray = jsonObject["results"].AsArray();
         var htmlGenerator = new HtmlBuilder();
         htmlGenerator.OpenTag("table");
         foreach (var pokemon in pokemonArray)
         {
-            BuildSinglePokemon(ref htmlGenerator, pokemon);
+            BuildSinglePokemon(ref htmlGenerator, subPageDirectory, pokemon);
         }
 
         htmlGenerator.CloseTag();
         Console.WriteLine(htmlGenerator.ToString());
     }
 
-    private static void BuildSinglePokemon(ref HtmlBuilder htmlGenerator, JsonNode pokemon)
+    private static void BuildSinglePokemon(ref HtmlBuilder mainPageGenerator, string subPageDirectory, JsonNode pokemon)
     {
+        #region Get Data
+
         string index = pokemon["url"].ToString().Split("/")[6];
-        
+
         pokemon = FetchJson(pokemon["url"].ToString());
-        
+
         string pokedexNumber = $"#{index.PadLeft(3, '0')}";
         string name = pokemon["name"].ToString();
         string image =
@@ -40,42 +53,71 @@ class Program
         var stats = pokemon["stats"]
             .AsArray()
             .Select(stat =>
-        {
-            string statName = stat["stat"]["name"].ToString();
-            string baseStat = stat["base_stat"].ToString();
-            return $"{statName}: {baseStat}";
-        });
+            {
+                string statName = stat["stat"]["name"].ToString();
+                string baseStat = stat["base_stat"].ToString();
+                return $"{statName}: {baseStat}";
+            });
+        var locations = JsonObject.Parse(Fetch(pokemon["location_area_encounters"].ToString())).AsArray()
+            .Select(location => location["location_area"]["name"].ToString());
 
-        htmlGenerator.OpenTag("tr");
+        #endregion
 
-        htmlGenerator.OpenTag("td");
-        htmlGenerator.InsertText(pokedexNumber);
-        htmlGenerator.CloseTag();
+        #region Build main page content
 
-        htmlGenerator.OpenTag("td");
-        htmlGenerator.InsertText(image);
-        htmlGenerator.CloseTag();
+        mainPageGenerator.OpenTag("tr");
 
-        htmlGenerator.OpenTag("td");
-        htmlGenerator.InsertText(name);
-        htmlGenerator.CloseTag();
-        
-        htmlGenerator.OpenTag("td");
-        htmlGenerator.InsertText(type);
-        htmlGenerator.CloseTag();
-        
-        htmlGenerator.OpenTag("td");
-        htmlGenerator.OpenTag("ul");
+        mainPageGenerator.OpenTag("td");
+        mainPageGenerator.InsertText(pokedexNumber);
+        mainPageGenerator.CloseTag();
+
+        mainPageGenerator.OpenTag("td");
+        mainPageGenerator.InsertText(image);
+        mainPageGenerator.CloseTag();
+
+        mainPageGenerator.OpenTag("td");
+        mainPageGenerator.InsertText(name);
+        mainPageGenerator.CloseTag();
+
+        mainPageGenerator.OpenTag("td");
+        mainPageGenerator.InsertText(type);
+        mainPageGenerator.CloseTag();
+
+        mainPageGenerator.OpenTag("td");
+        mainPageGenerator.OpenTag("ul class=\"stats\"");
         foreach (var stat in stats)
         {
-            htmlGenerator.OpenTag("li");
-            htmlGenerator.InsertText(stat);
-            htmlGenerator.CloseTag();
+            mainPageGenerator.OpenTag("li");
+            mainPageGenerator.InsertText(stat);
+            mainPageGenerator.CloseTag();
         }
-        htmlGenerator.CloseTag();
-        htmlGenerator.CloseTag();
 
-        htmlGenerator.CloseTag();
+        mainPageGenerator.CloseTag();
+        mainPageGenerator.CloseTag();
+
+        mainPageGenerator.OpenTag("td");
+        mainPageGenerator.OpenTag("ul class=\"locations\"");
+        foreach (var location in locations)
+        {
+            mainPageGenerator.OpenTag("li");
+            mainPageGenerator.InsertText(location);
+            mainPageGenerator.CloseTag();
+        }
+
+        mainPageGenerator.CloseTag();
+        mainPageGenerator.CloseTag();
+
+        mainPageGenerator.CloseTag();
+
+        #endregion
+
+        #region Build sub page content
+
+        var subPageGenerator = new HtmlBuilder();
+
+        File.WriteAllText(Path.Combine(subPageDirectory, $"{name}.html"), subPageGenerator.ToString());
+
+        #endregion
     }
 
     private static string Fetch(string url)
@@ -93,9 +135,30 @@ class Program
         var jsonObject = JsonNode.Parse(content).AsObject();
         return jsonObject;
     }
+
     private static JsonObject FetchPokemon(int offset, int num)
     {
         return FetchJson($"https://pokeapi.co/api/v2/pokemon?offset={offset}&limit={num}");
+    }
+
+    private static bool ArgsValid(string[] args, out string mainPagePath, out string subPagePath,
+        out string stylesheetPath)
+    {
+        mainPagePath = string.Empty;
+        subPagePath = string.Empty;
+        stylesheetPath = string.Empty;
+
+        // Expected args: Main page path, Subpage directory path, Stylesheet path, Pokemon count
+        if (args.Length != 3)
+        {
+            Console.WriteLine("Invalid number of arguments.");
+            return false;
+        }
+
+        mainPagePath = args[0];
+        subPagePath = args[1];
+        stylesheetPath = args[2];
+        return true;
     }
 }
 
@@ -104,9 +167,21 @@ public class HtmlBuilder()
     private Stack<string> _tagStack = new();
     private System.Text.StringBuilder _htmlBuilder = new();
 
+    public void OpenDocument(string title, string? stylesheet = null)
+    {
+        string stylesheetLink = stylesheet != null ? $"<link rel=\"stylesheet\" href=\"{stylesheet}\">" : string.Empty;
+        _htmlBuilder.Append(
+            $"<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>{title}</title>{stylesheetLink}</head><body>");
+    }
+
+    public void CloseDocument()
+    {
+        _htmlBuilder.Append("</body></html>");
+    }
+
     public void OpenTag(string tag)
     {
-        _tagStack.Push(tag);
+        _tagStack.Push(tag.Split(' ')[0]);
         _htmlBuilder.Append($"<{tag}>");
     }
 
