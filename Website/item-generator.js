559b59329f5fdede89a2247210aa20a0c193293b
luckylukii <lukas.gruenzweil@liwest.at>
2025-01-16 20:00:10 +0100

    Added category headers

--- Website/item-generator.js
+++ Website/item-generator.js
@@ -6,11 +6,19 @@ document.addEventListener('DOMContentLoaded', async function() {
         const response = await fetch(`https://pokeapi.co/api/v2/item?limit=${numItems}`);
         const data = await response.json();
 
+        const categories = {};
+
         for (const item of data.results) {
+            const itemData = await (await fetch(item.url)).json();
+            const category = itemData.category.name;
+
+            if (!categories[category]) {
+                categories[category] = [];
+            }
+
+            const description = await getDescriptions(itemData);
             const itemElement = document.createElement('div');
             itemElement.classList.add('item');
-
-            const description = await getDescriptions(item);
             itemElement.innerHTML = `
                 <div class="item-image">
                     <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/${item.name}.png" alt="${item.name}">
@@ -19,7 +27,18 @@ document.addEventListener('DOMContentLoaded', async function() {
                     <h2>${formatName(item.name)}</h2>
                     <p>${description}</p>
                 </div>`;
-            container.appendChild(itemElement);
+            categories[category].push(itemElement);
+        }
+
+        for (const [category, items] of Object.entries(categories)) {
+            const categoryElement = document.createElement('h1');
+            categoryElement.innerHTML = formatName(category);
+            container.appendChild(categoryElement);
+            container.appendChild(document.createElement('br'));
+
+            items.forEach(itemElement => {
+                container.appendChild(itemElement);
+            });
         }
     } catch (error) {
         console.error('Error fetching items:', error);
@@ -30,8 +49,7 @@ function formatName(name) {
     return name.charAt(0).toUpperCase() + name.slice(1);
 }
 
-async function getDescriptions(item) {
-    const response = await fetch(item.url);
-    const data = await response.json();
-    return data.effect_entries[0].effect;
+async function getDescriptions(data) {
+    const length = data.effect_entries[0].effect.length;
+    return length > 200 ? data.effect_entries[0].short_effect : data.effect_entries[0].effect;
 }
